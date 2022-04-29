d9e1764b271d29e0e5f636709d83babd5f407033
Alex F <acflint@users.noreply.github.com>
2022-04-29 15:12:35 -0700

    modifying jfu callbacks

--- app/assets/javascripts/activeadmin/jfu_upload_input.js
+++ app/assets/javascripts/activeadmin/jfu_upload_input.js
@@ -8,11 +8,19 @@ $(document).ready( function() {
         data.context.focus();
       },
       done: function(e, data) {
-        $(e.target).next().html('<li class="jfu_info jfu_done"> Upload complete! </li>');  // data.result.file_name
+        // $(e.target).next().html('<li class="jfu_info jfu_done"> Upload complete! </li>');
+        // data.result.file_name
       },
       progressall: function(e, data) {
         var progress = parseInt(data.loaded / data.total * 100, 10);
         $(e.target).next().html('<li class="jfu_info jfu_progress" style="width:' + progress + '%"> ' + progress + '% ... </li>');
+        if (progress == 100) {
+          $(e.target).next().html('<li class="jfu_info jfu_done"> Upload complete! </li>');
+        }
+      },
+      progress: function(e, data) {
+        console.log('progress e', e);
+        console.log('progress data', data);
       }
     };
     options = $.extend({}, options, $(this).data('options'));
